5bbb20b1922e0db5633b87b457d4b7a9e8e3795d
pythearn <ermandmani@gmail.com>
2022-07-26 14:03:52 +0100

    first commit

--- db/task_manager.sql
+++ db/task_manager.sql
@@ -1,9 +1,25 @@
 DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS users;
+
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR,
+  last_name  VARCHAR
+
+
+)
+
 
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
   description VARCHAR(255),
-  assignee VARCHAR(255),
+  -- assignee VARCHAR(255),
   duration INT,
   completed BOOLEAN DEFAULT FALSE
+  user_id IN REFERENCES users(id)
+  
 );
+
+
